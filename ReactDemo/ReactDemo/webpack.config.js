1ba46730ee26755339a96b84125bc1765bc6da04
马鑫 <anyangmaxin@163.com>
2016-09-28 18:16:23 +0800

    以上内容来自于 https://hulufei.gitbooks.io/react-tutorial/content/webpack.html   下节内容   https://hulufei.gitbooks.io/react-tutorial/content/jsx.html

---
+++ ReactDemo/ReactDemo/webpack.config.js
@@ -0,0 +1,16 @@
+﻿var path = require('path');
+module.exports = {
+    entry: './entry.js',
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename:'bundle.js'
+    },
+    resolve: {
+        extensions:['','.js','.jsx']
+    },
+    module: {
+        loaders: [
+            {test:/\.js|jsx$/,loaders:['babel']}
+        ]
+    }
+};
