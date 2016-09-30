bd10a36fd6719bb841b54383b8b9281e5c56a765
马鑫 <anyangmaxin@163.com>
2016-09-30 11:00:45 +0800

    webpack  打包命令不成功 ，不知道怎么弄了。

---
+++ ReactDemo/React_Webpack/webpack.config.js
@@ -0,0 +1,37 @@
+﻿module.exports= {
+    entry: "./src/index.tsx",
+    output: {
+        path:"/dist",
+        filename:"bundle.js"
+    },
+    //Enable sourcemaps for debugging webpack`s output.
+    devetool: "source-map",
+    resolve: {
+        //Add '.ts'  and '.tsx' as resolvable extensions.
+        extensions:["",".webpack.js",".web.js",".ts",".tsx",".js"]
+    },
+    module: {
+        loaders: [
+        //All files with a '.ts'  or '.tsx'  extension will be handled by 'ts-loader'.
+            {test:/\.tsx?$/,loader:"ts-loader"}
+        ],
+        preLoaders: [
+            //All output '.js'  files will have any sourcemaps re-processed by 'source-map-loader'.
+            {test:/\.js$/,loader:"source-map-loader"}
+        ]
+    },
+
+
+
+    /**
+     *     
+     when importing a module whose path mathches one of the following,just
+    assume a corresponding global varibale exists and use that instead.
+    this is important because it allows us to avoid bundling all of our
+    dependencies,which allows browsers to cache those libraries between builds.
+    */
+    externals: {
+        "react": "React",
+        "react-dom":"ReactDOM"
+    }
+}
